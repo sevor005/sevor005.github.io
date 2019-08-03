acdfdef911bae3c853df62a63b976329420a0dce
sevor005 <sevor005@yandex.ru>
2019-08-03 17:07:37 +0700

    добавление файлов

---
+++ precache-manifest.45bb23d8fbee43baa257b202d8411bec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "580f87ed1195044b6726",
+    "url": "/static/js/main.b3bffd5b.chunk.js"
+  },
+  {
+    "revision": "a9f64fb58498f6baca48",
+    "url": "/static/js/2.c75c4f44.chunk.js"
+  },
+  {
+    "revision": "580f87ed1195044b6726",
+    "url": "/static/css/main.f7913db5.chunk.css"
+  },
+  {
+    "revision": "116fe6c504a783ac2a41298968b089d5",
+    "url": "/index.html"
+  }
+];
